a4c1330370f97d9c55226abf339526b9e9fba618
Estelle Choi <estele.choi@gmail.com>
2022-03-07 16:01:01 +0900

    lint: files linted

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,18 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:json/recommended"
-    ],
-    plugins: ["prettier"],
-    parserOptions: {
-      sourceType: 'module',
-      ecmaVersion: 2020,
-    },
-    rules: {
-      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-      // https://github.com/prettier/eslint-plugin-prettier#options
-      "prettier/prettier": [
-        "error", 
-        {}, 
-        { usePrettierrc: true, }
-      ],
-    },
-  };
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: ['eslint:recommended', 'plugin:json/recommended'],
+	plugins: ['prettier'],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+	},
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		// https://github.com/prettier/eslint-plugin-prettier#options
+		'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+	},
+}
