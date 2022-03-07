e662ab0b2549a114e32a414e7a319ca2242b2343
Estelle Choi <estele.choi@gmail.com>
2022-03-07 16:00:39 +0900

    config: husky initialized for this project

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     extends: [
       "eslint:recommended",
+      "plugin:json/recommended"
     ],
     plugins: ["prettier"],
     parserOptions: {
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-	'*.{js,ts,vue},*rc.js': ['yarn lint'],
-	'{!(package)*.json,*.code-snippets}': ['yarn lint:prettier --parser json'],
-	'package.json': ['yarn lint:prettier --parser json'],
-	'*.scss': ['yarn lint'],
+	'*.{js,ts},*rc.js': ['yarn lint'],
+	'{!(package)*.json,*.code-snippets}': ['yarn lint'],
+	'package.json': ['yarn lint'],
 }
